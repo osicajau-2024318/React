769c47ce069bc68128ca446142eff273a1096a9c
Humberto-crl <hperez-2022161@kinal.edu.gt>
2026-03-25 13:16:49 -0600

    feat: new rama

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,3 +9,15 @@ export const Saludo = ({nombre,apellido, edad}) => {
     </div>
   )
 }   
+
+/*import React from 'react'
+
+export const Saludo = ({nombre,apellido, edad}) => {
+  return (
+    <div>
+        <h2>Hola, soy {nombre}{apellido}</h2>
+        <p>Tengo {edad} años</p>
+
+    </div>
+  )
+}    */
